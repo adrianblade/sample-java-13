3be98638941c8b86410c39f5015683a75b3474c8
Adrian Catalan <adrian.catalan@travel-ims.com>
2019-12-27 20:05:42 +0100

    Initial selenium test

--- server/src/main/java/com/adr/example/ExampleApplication.java
+++ server/src/main/java/com/adr/example/ExampleApplication.java
@@ -2,6 +2,7 @@ package com.adr.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ExampleApplication {
@@ -9,5 +10,4 @@ public class ExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
-
 }
---
+++ server/src/test/java/com/adr/example/GoogleSearchPageTraditionalSeleniumTests.java
@@ -0,0 +1,22 @@
+package com.adr.example;
+
+import static org.junit.Assert.assertNotNull;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GoogleSearchPageTraditionalSeleniumTests extends BaseSeleniumTests {
+
+    @Test
+    public void getSearchPage() {
+        this.driver.get("https://www.google.com");
+        WebElement element = this.driver.findElement(By.name("q"));
+        assertNotNull(element);
+    }
+
+}
